a47c9183d5e76a74220b111195198e600ed8d127
Zachary Hirsch <zhirsch@umich.edu>
2026-01-09 09:38:34 -0500

    Fix SMB getting stuck at the title screen.

--- src/main/java/com/zacharyhirsch/moldynes/emulator/ppu/NesPpuSprite.java
+++ src/main/java/com/zacharyhirsch/moldynes/emulator/ppu/NesPpuSprite.java
@@ -3,6 +3,6 @@ package com.zacharyhirsch.moldynes.emulator.ppu;
 record NesPpuSprite(int index, int y, int tileIndex, int attributes, int x) {
 
   public boolean valid() {
-    return y() != 0xff && tileIndex() != 0xff && attributes() != 0xff && x() != 0xff;
+    return y() != 0xff && x() != 0xff;
   }
 }
